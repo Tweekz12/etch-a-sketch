d470fcc3765b6d5b19518b5f9ac58d6f99c73b15
Tweekz <andreipetrea50@gmail.com>
2023-03-12 18:24:12 +0200

    Add scrips and styles to automatically generate the square and color them

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log('hello');
+const container = document.querySelector('.container');
+for (let i = 0; i < 256; i++) {
+    const div = document.createElement('div');
+    div.classList.add('square');
+    container.appendChild(div);
+}
+
+const squares = document.querySelectorAll('.square');
+squares.forEach(square => square.addEventListener('mouseover', (e) => {
+    e.target.classList.add('colored');
+}));

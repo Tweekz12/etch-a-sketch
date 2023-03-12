b91e2dd2c2df2b5675f185b0770bd41b5c801993
Tweekz <andreipetrea50@gmail.com>
2023-03-12 18:32:51 +0200

    Move the generation of the grid in a function

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
-const container = document.querySelector('.container');
-for (let i = 0; i < 256; i++) {
-    const div = document.createElement('div');
-    div.classList.add('square');
-    container.appendChild(div);
+function drawSquares(heightOfOneSquare) {
+    const container = document.querySelector('.container');
+    for (let i = 0; i < heightOfOneSquare * heightOfOneSquare; i++) {
+        const div = document.createElement('div');
+        div.classList.add('square');
+        container.appendChild(div);
+    }
 }
+
+drawSquares(16);
 
 const squares = document.querySelectorAll('.square');
 squares.forEach(square => square.addEventListener('mouseover', (e) => {
